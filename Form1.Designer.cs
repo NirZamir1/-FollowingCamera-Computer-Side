c780c155faea83005a6ee9606ba5659387b4039e
NirZamir <nirozamir@gmail.com>
2021-11-14 18:53:54 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace emguCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.Device = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(697, 69);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 23);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(80, 25);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(611, 23);
+            this.cboDevice.TabIndex = 1;
+            this.cboDevice.SelectedIndexChanged += new System.EventHandler(this.cboDevice_SelectedIndexChanged);
+            // 
+            // Device
+            // 
+            this.Device.AutoSize = true;
+            this.Device.Location = new System.Drawing.Point(29, 28);
+            this.Device.Name = "Device";
+            this.Device.Size = new System.Drawing.Size(45, 15);
+            this.Device.TabIndex = 2;
+            this.Device.Text = "Device:";
+            this.Device.Click += new System.EventHandler(this.Device_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(80, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(611, 317);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Device);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Face Detection";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.Label Device;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using emguCV;
+namespace emguCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        private void Device_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo info in filter)
+            {
+                cboDevice.Items.Add(info.Name);
+            }
+            cboDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[cboDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier(@"C:\Users\PC\source\repos\emguCV\haarcascade_frontalface_alt_tree.xml");
+
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = bitmap.ToImage<Bgr, byte>();
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            foreach(Rectangle rec in rectangles)
+            {
+                using(Graphics g=Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen=new Pen(Color.Red,1))
+                    {
+                        g.DrawRectangle(pen, rec);
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(device.IsRunning)
+            {
+                device.Stop();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
